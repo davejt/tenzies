c1d9aa525d2e7683f73898fa7a90f96918ab1754
davejt <djtuddenham@googlemail.com>
2022-06-13 08:43:07 +0100

    Display the dots on the dice instead of the number

--- src/Die.js
+++ src/Die.js
@@ -1,12 +1,30 @@
 import React from "react"
 
 export default function Die(props) {
+    const dotsArray = [
+        [false, false, false, false, true, false, false, false, false],
+        [true, false, false, false, false, false, false, false, true],
+        [true, false, false, false, true, false, false, false, true],
+        [true, false, true, false, false, false, true, false, true],
+        [true, false, true, false, true, false, true, false, true],
+        [true, false, true, true, false, true, true, false, true]
+    ]
     const styles = {
         backgroundColor: props.held ? "#59E391" : "white"
     }
+
+    const currentDie = dotsArray[props.value-1]
+
+    const dotsElements = currentDie.map(dot => (
+        dot ? <span className='die-dot on' /> : <span className='die-dot off' />
+    ))
+
+
     return (
         <div className="die-face" onClick={props.hold} style={styles}>
-            <h2 className="die-num">{props.value}</h2>
+            <div className="dot-container">
+                {dotsElements}
+            </div>
         </div>
     )
 }
